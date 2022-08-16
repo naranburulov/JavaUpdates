ef62ec339f486c4278e002428ef19cc636db0c15
naranburulov <naranburulov@gmail.com>
2022-08-16 09:53:53 -0400

    enabled lombok

--- m3-lambda/src/main/java/com/cydeo/Apple.java
+++ m3-lambda/src/main/java/com/cydeo/Apple.java
@@ -1,42 +1,18 @@
 package com.cydeo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
 public class Apple {
 
     private int weight;
     private Color color;
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    @Override
-    public String toString() {
-        return "Apple{" +
-                "weight=" + weight +
-                ", color=" + color +
-                '}';
-    }
-
-    public Apple(int weight, Color color) {
-        this.weight = weight;
-        this.color = color;
-
-
-
-
-
-    }
 
 }
