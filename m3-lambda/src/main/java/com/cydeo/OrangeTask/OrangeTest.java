358e6ef2f428970a99c6af6f37c27e492d286df2
naranburulov <naranburulov@gmail.com>
2022-08-17 05:37:37 -0400

    critical method using the inventory list of oranges and OrangeFormatter interface was created in the TestOrange class

--- m3-lambda/src/main/java/com/cydeo/OrangeTask/OrangeTest.java
+++ m3-lambda/src/main/java/com/cydeo/OrangeTask/OrangeTest.java
@@ -1,15 +1,12 @@
 package com.cydeo.OrangeTask;
 
-import com.cydeo.appleTask.Apple;
-import com.cydeo.appleTask.Color;
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class OrangeTest {
 
     public static void main(String[] args) {
 
-        List<Orange> oranges = new ArrayList<>();
+        List<Orange> inventory = new ArrayList<Orange>();
         inventory.add(new Orange(300, com.cydeo.OrangeTask.Color.GREEN));
         inventory.add(new Orange(100, com.cydeo.OrangeTask.Color.RED));
         inventory.add(new Orange(200, com.cydeo.OrangeTask.Color.GREEN));
@@ -18,4 +15,13 @@ public class OrangeTest {
 
 
     }
+
+    private static void prettyPrintOrange(List<Orange> inventory, OrangeFormatter formatter){
+        for(Orange orange : inventory){
+            String output = formatter.accept(orange);
+            System.out.println(output);
+        }
+    }
+
+
 }
