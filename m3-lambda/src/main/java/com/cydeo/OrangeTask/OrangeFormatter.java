6acd15138553849a0051d0bee21fa55d23e7399c
naranburulov <naranburulov@gmail.com>
2022-08-17 05:05:48 -0400

    functional interface created, main class created

---
+++ m3-lambda/src/main/java/com/cydeo/OrangeTask/OrangeFormatter.java
@@ -0,0 +1,10 @@
+package com.cydeo.OrangeTask;
+
+@FunctionalInterface             //lambda can be used
+public interface OrangeFormatter {
+    String accept(Orange orange);   //because task requires String output (or to return String)
+
+}
+/*
+why? Because different behaviours to be created
+ */
---
+++ m3-lambda/src/main/java/com/cydeo/OrangeTask/OrangeTest.java
@@ -0,0 +1,21 @@
+package com.cydeo.OrangeTask;
+
+import com.cydeo.appleTask.Apple;
+import com.cydeo.appleTask.Color;
+
+import java.util.ArrayList;
+
+public class OrangeTest {
+
+    public static void main(String[] args) {
+
+        List<Orange> oranges = new ArrayList<>();
+        inventory.add(new Orange(300, com.cydeo.OrangeTask.Color.GREEN));
+        inventory.add(new Orange(100, com.cydeo.OrangeTask.Color.RED));
+        inventory.add(new Orange(200, com.cydeo.OrangeTask.Color.GREEN));
+        inventory.add(new Orange(50, Color.RED));
+
+
+
+    }
+}
