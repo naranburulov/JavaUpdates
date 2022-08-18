4bbb3bf48d7c656b1a852cabafad86783a72f4b5
naranburulov <naranburulov@gmail.com>
2022-08-18 08:53:26 -0400

    SocialNetworkTask - work started

---
+++ m3-lambda/src/main/java/com/cydeo/socialNetworkTask/Person.java
@@ -0,0 +1,19 @@
+package com.cydeo.socialNetworkTask;
+
+public class Person {
+
+    private String name;
+    private int age;
+    //private Gender gender;
+    private String emailAddress;
+
+}
+/*
+Suppose that you are creating a social networking application.
+You want to create a feature that enables an administrator
+to perform any kind of action,
+such as sending a message to the members of the social networking
+application that satisfy certain criteria:
+
+Send message to Male Members whose age is between 18 and 25
+ */
