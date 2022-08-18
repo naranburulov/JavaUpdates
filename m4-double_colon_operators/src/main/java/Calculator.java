edbf27cc4f2362443b6e4481f2203b66e9117a67
naranburulov <naranburulov@gmail.com>
2022-08-18 11:58:51 -0400

    more examples added

--- m4-double_colon_operators/src/main/java/Calculator.java
+++ m4-double_colon_operators/src/main/java/Calculator.java
@@ -1,11 +1,11 @@
 public class Calculator {
 
     public static void findSum(int x, int y) {
-        System.out.println( "Sum " +(x+y) );
+        System.out.println( "Sum " + (x + y) );
     }
 
     public void findMultiply(int x, int y){
-        System.out.println( "Multiply " + (x*y)  );
+        System.out.println( "Multiply " + (x * y)  );
     }
 
 
