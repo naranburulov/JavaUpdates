d8522dbbbaac4f55a1ed598dece646dfb898807b
naranburulov <naranburulov@gmail.com>
2022-08-18 08:58:14 -0400

    lombok added to Person class, Gender enum created

---
+++ m3-lambda/src/main/java/com/cydeo/socialNetworkTask/Gender.java
@@ -0,0 +1,5 @@
+package com.cydeo.socialNetworkTask;
+
+public enum Gender {
+    MALE, FEMALE;
+}
--- m3-lambda/src/main/java/com/cydeo/socialNetworkTask/Person.java
+++ m3-lambda/src/main/java/com/cydeo/socialNetworkTask/Person.java
@@ -1,10 +1,20 @@
 package com.cydeo.socialNetworkTask;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Person {
 
     private String name;
     private int age;
-    //private Gender gender;
+    private Gender gender;
     private String emailAddress;
 
 }
